d06311ad1e5dad13d6e2c8d08f2bb285a445ef34
zoequimey25 <zoequimey25@gmail.com>
2026-05-14 20:36:50 -0300

    practicas

---
+++ 1c26/.for.py
@@ -0,0 +1,11 @@
+for n in range (1,11):
+    print(n)
+
+for n in range (10,0,-1):
+    print(n)
+numero= int(input("Ingrese un numero a contar: "))
+for n in range(0,numero+1):
+    print(n)
+n=int(input("Ingrese un numero a multiplicar: "))
+for j in range (0,11):
+    print (f"{n} x {j} = {n*j}")
